374f91c255ef3b81d01e1681b61eeda434a0c435
Guilherme Sitton de Carvalho <132395003+Gui-Sitton@users.noreply.github.com>
2025-03-27 10:18:30 -0300

    Update Dash.py

--- Dash.py
+++ Dash.py
@@ -37,8 +37,10 @@ if authentication_status:
         # 🔹 Inicializar Firebase no Streamlit
     if not firebase_admin._apps:
         # Carregar as credenciais JSON do Streamlit secrets
-        firebase_cred = json.loads(st.secrets["firebase"]["credentials"])  # Carrega o JSON a partir da string
-        cred = credentials.Certificate(firebase_cred)
+        firebase_cred = st.secrets["firebase"]["credentials"]
+        cred = credentials.Certificate(json.loads(firebase_cred))
+        
+        
         firebase_admin.initialize_app(cred)
 
     db = firestore.client()
